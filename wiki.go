90f3d7412dc38ecacf4c471e5e9413a1a7b2a76e
dpolandstarn <dpolandstarn@dpsbuilds.dev>
2026-06-29 17:01:40 -0500

    Added net/http to run and host a simple server serving a test document with Hello world

--- wiki.go
+++ wiki.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	http.HandleFunc("/view/", viewHandler)
 
-	p1.save()
-
-	p2, _ := loadPage("TestPage")
-
-	fmt.Println(string(p2.Body))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+// Page
 
 type Page struct {
 	Title string
